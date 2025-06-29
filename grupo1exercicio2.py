255a69ab3e1f450d9f81f0b42cb46853f459cd80
MarcDevGuy127 <marceloh.github@gmail.com>
2025-06-29 18:43:52 -0300

    Resolução #1 grupo1exercicio2

---
+++ grupo1exercicio2.py
@@ -0,0 +1,11 @@
+#2. Calcule a média de três números informados pelo usuário.
+
+pNota = float(input("Primeira nota:"))
+
+sNota = float(input("Segunda nota:"))
+
+tNota = float(input("Terceira nota:"))
+
+media = pNota + sNota + tNota/3
+
+print("A media e:", media)
