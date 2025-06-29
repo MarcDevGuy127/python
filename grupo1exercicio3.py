8a62e3dab0f2e2fd56740ba378dd11e1cc6db52f
MarcDevGuy127 <marceloh.github@gmail.com>
2025-06-29 19:08:13 -0300

    Resolução #1 grupo1exercicio3

---
+++ grupo1exercicio3.py
@@ -0,0 +1,7 @@
+#3. Converta uma temperatura de Celsius para Fahrenheit.
+
+celsius = float(input("Digite um valor em °C:"))
+
+farenheit = (celsius * 9 / 5) + 32
+
+print(f"{celsius}°C -> {farenheit}°F")
