cf5346a5d5ed266a2d663a552e0a96db7139226f
MarcDevGuy127 <marceloh.github@gmail.com>
2025-06-29 18:23:55 -0300

    Resolução #1 grupo1exercicio1

---
+++ grupo1exercicio1.py
@@ -0,0 +1,9 @@
+#1. Escreva um programa que peça ao usuário seu nome e idade e exiba uma mensagem personalizada.
+
+print("Escreva o seu nome:")
+nome = input()
+
+print("Digite a sua idade:")
+idade = input()
+
+print(f"Nome: {nome} Idade: {idade}")
